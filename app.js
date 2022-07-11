c47c0e904aa2040636b062c729a4ae65a9d8bb9a
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-07-11 07:30:03 +0530

    test 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const db = require("./models");
 app.use(bodyParser.json())
 
 const corsOptions ={
-   origin:'https://62cb80bfd203bd2affb204de--friendly-rolypoly-7fa8e8.netlify.app',
+   origin:'https://62cb83333563890008931b51--friendly-rolypoly-7fa8e8.netlify.app',
    credentials:true,            //access-control-allow-credentials:true
    optionSuccessStatus:200,
 }
